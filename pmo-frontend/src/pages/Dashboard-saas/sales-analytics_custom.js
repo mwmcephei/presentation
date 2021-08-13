54322793c22e67a35475d111e5aeea43fc47cb7e
matthias müller <mwm@cephei.com>
2021-08-13 18:56:47 +0200

    green dot not cut off anymore on small screen

--- pmo-frontend/src/pages/Dashboard-saas/sales-analytics_custom.js
+++ pmo-frontend/src/pages/Dashboard-saas/sales-analytics_custom.js
@@ -20,56 +20,56 @@ const SalesAnalytics = props => {
 
   return (
     <React.Fragment>
-        <Card>
-          <CardBody>
-            <h4 className="card-title mb-4">{props.title}</h4>
+      <Card>
+        <CardBody>
+          <h4 className="card-title mb-4">{props.title}</h4>
 
-            <div>
-              <div id="donut-chart">
-                <ReactApexChart
-                  options={options}
-                  series={series}
-                  type="donut"
-                  height={260}
-                  className="apex-charts"
-                />
-              </div>
+          <div>
+            <div id="donut-chart">
+              <ReactApexChart
+                options={options}
+                series={series}
+                type="donut"
+                height={260}
+                className="apex-charts"
+              />
             </div>
+          </div>
 
-            <div className="text-center text-muted">
-              <Row>
+          <div className="text-center text-muted">
+            <Row>
               <Col xs="4">
-                  <div className="mt-4">
-                    <p className="mb-2 ">
-                      <span>Behind </span>
-                      <i className="mdi mdi-circle text-danger me-1 " />
-                    </p>
-                    <h5>{props.red}</h5>
-                  </div>
-                </Col>
-                <Col xs="4">
-                  <div className="mt-4">
-                    <p className="mb-2 text-truncate">
-                    <span>On Track </span> 
-                      <i className="mdi mdi-circle text-warning me-1" />
-                    </p>
-                    <h5>{props.yellow}</h5>
-                  </div>
-                </Col>
-                <Col xs="4">
-                  <div className="mt-4">
-                    <p className="mb-2 text-truncate">
-                      <span>Achieved </span> 
-                      <i className="mdi mdi-circle text-success me-1" /> 
-                    </p>
-                    <h5>{props.green}</h5>
-                  </div>
-                </Col>
-                
-              </Row>
-            </div>
-          </CardBody>
-        </Card>
+                <div className="mt-4">
+                  <span className="mb-2 ">
+                    <span>Behind </span>
+                    <i className="mdi mdi-circle text-danger me-1 " />
+                  </span>
+                  <h5 className="mt-2">{props.red}</h5>
+                </div>
+              </Col>
+              <Col xs="4">
+                <div className="mt-4">
+                  <span className="mb-2 text-truncate">
+                    <span>On Track </span>
+                    <i className="mdi mdi-circle text-warning me-1" />
+                  </span>
+                  <h5 className="mt-2">{props.yellow}</h5>
+                </div>
+              </Col>
+              <Col xs="4">
+                <div className="mt-4">
+                  <span className="mb-2 text-truncate">
+                    <span >Achieved </span>
+                    <i className="mdi mdi-circle text-success me-1 " />
+                  </span>
+                  <h5 className="mt-2">{props.green}</h5>
+                </div>
+              </Col>
+
+            </Row>
+          </div>
+        </CardBody>
+      </Card>
     </React.Fragment>
   )
 }
