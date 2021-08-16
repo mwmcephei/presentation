8090d26ba0a773f2a13de99b769b806e3ae6ff8d
matthias müller <mwm@cephei.com>
2021-08-16 17:38:51 +0200

    adjusted line breaks and spacing

--- pmo-frontend/src/pages/Dashboard-saas/card-user_custom.js
+++ pmo-frontend/src/pages/Dashboard-saas/card-user_custom.js
@@ -11,16 +11,16 @@ import {
 import avatar1 from "../../assets/images/users/avatar-1.jpg"
 
 const getCircle = (number) => {
-  switch(number) {
-      case 0:
-        return (<div className="justify-content-center mx-auto text-center rounded-circle bg-success  text-muted text-truncate" style={{height:'30px', width:'30px'}} ></div>)
-      case 1:
-        return (<div className="justify-content-center mx-auto text-center rounded-circle bg-warning align-self-centerr" style={{height:'30px', width:'30px'}} ></div>)
-      case 2:
-          return (<div className="justify-content-center mx-auto text-center rounded-circle bg-danger align-self-center" style={{height:'30px', width:'30px'}} ></div>)
-      default: 
-        return (<div className="text-center rounded-circle " style={{height:'30px', width:'30px'}} ></div>)
-    }
+  switch (number) {
+    case 0:
+      return (<div className="justify-content-center mx-auto text-center rounded-circle bg-success  text-muted text-truncate" style={{ height: '30px', width: '30px' }} ></div>)
+    case 1:
+      return (<div className="justify-content-center mx-auto text-center rounded-circle bg-warning align-self-centerr" style={{ height: '30px', width: '30px' }} ></div>)
+    case 2:
+      return (<div className="justify-content-center mx-auto text-center rounded-circle bg-danger align-self-center" style={{ height: '30px', width: '30px' }} ></div>)
+    default:
+      return (<div className="text-center rounded-circle " style={{ height: '30px', width: '30px' }} ></div>)
+  }
 }
 
 
@@ -40,6 +40,17 @@ function CardUser(props) {
     width: props.kpiProgress + "%"
   };
 
+  let toalBudget_adjustedForDisplay;
+  if (props.budget > 1000000) {
+    const millions_float = props.budget / 1000000
+    const millions = Math.floor(millions_float)
+    const decimals = Math.floor((millions_float - millions) * 100)
+    toalBudget_adjustedForDisplay = millions + "." + decimals + "M"
+  } else {
+    toalBudget_adjustedForDisplay = props.budget
+  }
+
+
   return (
     <React.Fragment>
       <Row>
@@ -49,49 +60,52 @@ function CardUser(props) {
               <CardTitle className="mb-4">Short Summary</CardTitle>
               <Row>
                 <div>
-            
+
                   <div className="text-lg-center mt-4 mt-lg-0">
                     <Row>
                       <Col xs="4">
                         <div className="text-center">
-                          <p className="text-muted text-truncate mb-2">
+                          <p className="text-muted mb-2">
                             Total Measures
                           </p>
-                          <h5 className="mb-0"> {props.numberOfMeasures} </h5>
+                          <div className="mt-3">
+                            <h5 className="mb-0"> {props.numberOfMeasures} </h5>
+                          </div>
+
                         </div>
                       </Col>
                       <Col xs="4">
                         <div >
-                          <p className="text-muted text-truncate mb-2 text-center">
+                          <p className="text-muted mb-2 text-center">
                             Approved Budget
                           </p>
-                          <div className="text-center">
-                            <h5 className="mb-0 align-self-center"> {props.budget} € </h5>
+                          <div className="text-center mt-3">
+                            <h5 className="mb-2 align-self-center"> {toalBudget_adjustedForDisplay} € </h5>
                           </div>
-                          
+
                         </div>
                       </Col>
 
                       <Col xs="4" className="">
                         <div className="text-center">
-                          <p className="text-muted text-truncate mb-2">
+                          <p className="text-muted mb-2">
                             Overall Status
                           </p>
-                          <Row className="text-center">
-                            <Col className="justify-content-center mx-auto">
+                          <div className="text-center ">
+                            <div className="justify-content-center mx-auto">
                               {getCircle(props.signal)}
-                            </Col>
-                            
-                          </Row>
-                          
-                          
+                            </div>
+
+                          </div>
+
+
                         </div>
                       </Col>
                     </Row>
 
 
-                    <Row>
-                    <div>
+                    <Row className="mt-3">
+                      <div>
                         <ul className="list-group list-group-flush">
 
                           <li className="list-group-item">
@@ -120,7 +134,7 @@ function CardUser(props) {
                                 <div
                                   className="progress-bar bg-success"
                                   role="progressbar"
-                                  style={kpiProgress_style} 
+                                  style={kpiProgress_style}
                                   aria-valuenow={"" + props.kpiProgress}
                                   aria-valuemin="0"
                                   aria-valuemax="100"
@@ -132,7 +146,7 @@ function CardUser(props) {
                       </div>
                     </Row>
                   </div>
-                
+
                 </div>
               </Row>
             </CardBody>
